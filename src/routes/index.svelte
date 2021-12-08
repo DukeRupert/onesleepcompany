724fbd2b42ae66e769fef483e41d88b90897ab7f
Logan Williams <logan@firefly.llc>
2021-12-08 08:02:47 -0800

    Added sanity

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,24 @@
+
+<script context="module">
+	export async function load({ fetch }) {
+		const res = await fetch(`/api/home.json`);
+
+		if (res.ok) {
+			const { data } = await res.json();
+			return { props: { data } };
+		}
+
+		return {
+			status: res.status,
+			error: new Error()
+		};
+	}
+</script>
+
+<script lang="ts">
+    export let data
+    console.log(data)
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
