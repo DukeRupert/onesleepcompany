4d2b35ebf89de19ec2ba0147e13f0c5942a023d9
Logan Williams <logan@firefly.llc>
2021-12-08 19:17:39 -0800

    Created route about-us

---
+++ src/routes/about-us.svelte
@@ -0,0 +1,103 @@
+<div class="py-16 bg-white overflow-hidden">
+	<div class="max-w-8xl mx-auto px-4 space-y-8 sm:px-6 lg:px-8">
+		<div class="text-base max-w-prose mx-auto lg:max-w-none">
+			<h2 class="text-base text-primary font-semibold tracking-wide uppercase">About Us</h2>
+			<p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+				What makes us different
+			</p>
+		</div>
+		<div class="relative z-10 text-base max-w-prose mx-auto lg:max-w-5xl lg:mx-0 lg:pr-72">
+			<p class="text-lg text-gray-500">
+				Welcome to One Sleep Company! We are a Pacific Northwest based retail and online mattress
+				company selling a quality product at an affordable price. Every mattress in our lineup is
+				proudly made in the U.S.A. with only the best materials that you also find in major
+				manufactured brands. Our ownership began with two family friends who recognized a major flaw
+				in the mattress industry: <span class="font-medium md:font-normal"
+					>People are paying way too much money for a quality mattress.</span
+				>
+			</p>
+		</div>
+		<div class="lg:grid lg:grid-cols-2 lg:gap-8 lg:items-start">
+			<div class="relative z-10">
+				<div class="prose prose-indigo text-gray-500 mx-auto lg:max-w-none">
+					<p>
+						Joe Taylor and Chad Wittig came together with a simple vision: sell quality mattresses
+						at an affordable price. Prior to launching One Sleep Company, Chad worked in the
+						mattress industry for almost 10 years where he spent time building, designing, and
+						selling mattresses for a large manufacturing plant to local and regional retail mattress
+						stores. After working his way up from an entry level mattress builder to operations
+						manager of the entire plant Chad oversaw the production of thousands of mattresses daily
+						being built by more than 100 employees.
+					</p>
+
+					<p>
+						During this time, Chad realized he wanted to start his own business where he could help
+						the consumer get into a quality mattress at nearly half the price of the large retail
+						chains. Chad reached out to Joe Taylor, who owns a local HVAC company and explained to
+						him the business model with the ambition of teaming up as a partnership and making his
+						dream turn into a reality. With over 16 years of experience building a successful
+						customer service based business, Joe was the perfect match and with Chad they created
+						One Sleep Company. Today, Chad and Joe still live in Pierce County Washington where they
+						are both happily married and enjoy raising their children in the beautiful Pacific
+						Northwest.
+					</p>
+				</div>
+				<div class="mt-10 flex text-base max-w-prose mx-auto lg:max-w-none">
+					<div class="rounded-md shadow">
+						<a
+							href="/contact-us"
+							class="w-full flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-primary hover:bg-secondary"
+						>
+							Contact Sales
+						</a>
+					</div>
+					<div class="rounded-md shadow ml-4">
+						<a
+							href="/inventory?filter=mattresses"
+							class="w-full flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-primary bg-white hover:bg-gray-50"
+						>
+							Find a Mattress
+						</a>
+					</div>
+				</div>
+			</div>
+			<div class="mt-12 relative text-base max-w-prose mx-auto lg:mt-0 lg:max-w-none">
+				<blockquote class="relative bg-white rounded-lg shadow-lg">
+					<div class="rounded-t-lg px-6 py-8 sm:px-10 sm:pt-10 sm:pb-8">
+						<img src="logo-osc.svg" alt="One Sleep Company logo" class="h-8" />
+						<div class="relative text-lg text-gray-700 font-medium mt-8">
+							<svg
+								class="absolute top-0 left-0 transform -translate-x-3 -translate-y-2 h-8 w-8 text-gray-200"
+								fill="currentColor"
+								viewBox="0 0 32 32"
+								aria-hidden="true"
+							>
+								<path
+									d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z"
+								/>
+							</svg>
+							<p class="relative">People are paying way too much money for a quality mattress.</p>
+						</div>
+					</div>
+					<cite
+						class="relative flex items-center sm:items-start bg-primary rounded-b-lg not-italic py-5 px-6 sm:py-5 sm:pl-12 sm:pr-10 sm:mt-10"
+					>
+						<!-- <div
+							class="relative rounded-full border-2 border-white sm:absolute sm:top-0 sm:transform sm:-translate-y-1/2"
+						>
+							<img
+								class="w-12 h-12 sm:w-20 sm:h-20 rounded-full bg-indigo-300"
+								src="https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2.5&w=160&h=160&q=80"
+								alt=""
+							/>
+						</div> -->
+						<span class="relative ml-4 text-highlight font-semibold leading-6 sm:ml-24 sm:pl-1">
+							<p class="text-white font-semibold sm:inline">Chad Wittig</p>
+							<p class="sm:inline">Owner</p>
+						</span>
+					</cite>
+				</blockquote>
+			</div>
+		</div>
+	</div>
+</div>
