e0ec751f8cd672225368b62fb1668a45b03946f8
Logan Williams <logan@firefly.llc>
2021-12-08 17:16:13 -0800

    Added __layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,39 @@
-<script>
+<script lang="ts">
 	import '../app.css';
+	import Navbar from '$lib/components/navbar.svelte';
+	import Footer from '$lib/components/footer.svelte';
+	import Banner from '$lib/components/banner.svelte';
 </script>
 
-<slot />
+<div class="parent">
+	<nav class="bg-white shadow">
+		<Banner />
+		<Navbar />
+	</nav>
+	<main class="bg-white">
+		<slot />
+	</main>
+	<footer>
+		<Footer />
+	</footer>
+</div>
+
+<style>
+	.parent {
+		display: grid;
+		grid-template-rows: auto 1fr auto;
+	}
+	nav {
+		grid-row-start: 1;
+		grid-row-end: 2;
+	}
+	main {
+		grid-row-start: 2;
+		grid-row-end: 3;
+	}
+
+	footer {
+		grid-row-start: 3;
+		grid-row-end: 4;
+	}
+</style>
