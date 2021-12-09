bdbec1ecd96a8353a2632e8fdd23abd5df59a29f
Logan Williams <logan@firefly.llc>
2021-12-08 17:30:23 -0800

    Added Hero component

---
+++ src/lib/components/hero.ts
@@ -0,0 +1,15 @@
+export interface hero {
+	title: {
+		first: string;
+		second: string;
+	};
+	description: string;
+	image: {
+		url: string;
+		alt: string;
+	};
+	cta: {
+		title: string;
+		link: string;
+	};
+}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -16,9 +16,52 @@
 </script>
 
 <script lang="ts">
+	import { siteData } from "$lib/store";
+	import { urlFor } from "$lib/image-url";
+	import SvelteSeo from "svelte-seo";
+	import Hero from '$lib/components/hero.svelte'
+	import type {hero} from '$lib/components/hero'
     export let data
     console.log(data)
+
+	// SEO
+	const pageUrl = `https://${$page.host}${$page.path}`;
+
+	let heroData: hero = {
+		title: {
+			first: 'Wholesale pricing',
+			second: 'Factory direct'
+		},
+		description:
+			data.excerpt,
+		image: {
+			url: urlFor(data.mainImage.asset).format('webp').width(800).url(),
+			alt: data.mainImage.alt
+		},
+		cta: {
+			title: 'Shop Mattresses',
+			link: '/inventory/mattresses'
+		}
+	};
+
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<SvelteSeo
+	openGraph={{
+		title: $siteData.title + ' - ' + data.title,
+		description: data.excerpt,
+		url: pageUrl,
+		type: 'website',
+		images: [
+			{
+				url: urlFor(data.mainImage.asset).width(1200).height(627).format('webp').url(),
+				width: '1200',
+				height: '627',
+				alt: data.mainImage.alt
+			}
+		]
+	}}
+/>
+<div class="min-h-screen">
+	<Hero data={heroData} />
+</div>
